1874a4358a8c5f337c4763542e84cd9abf21ab55
Kilkun <kilkun@icloud.com>
2025-11-26 19:30:01 +0400

    feat: add cascade deletion logic

--- src/albums/album.module.ts
+++ src/albums/album.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AlbumsController } from './albums.controller';
 import { AlbumsService } from './albums.service';
+import { TracksModule } from 'src/tracks/tracks.module';
+import { FavoritesModule } from 'src/favorites/favorites.module';
 
 @Module({
+  imports: [forwardRef(() => TracksModule), forwardRef(() => FavoritesModule)],
   controllers: [AlbumsController],
   providers: [AlbumsService],
   exports: [AlbumsService],
