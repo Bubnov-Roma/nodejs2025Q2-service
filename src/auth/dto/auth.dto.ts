50ea60811f3d45fe346ab0c1a28c0062d724edb7
Kilkun <kilkun@icloud.com>
2025-11-26 18:43:50 +0400

    feat: implement JWT authentication and authorization

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,31 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class SignupDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(255)
+  login: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(30)
+  password: string;
+}
+
+export class LoginDto {
+  @IsString()
+  @IsNotEmpty()
+  login: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class RefreshDto {
+  @IsString()
+  @IsNotEmpty()
+  refreshToken: string;
+}
