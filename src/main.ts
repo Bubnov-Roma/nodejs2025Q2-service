8ee540cc1954a756b3c076b1cc50677478a1d016
Kilkun <kilkun@icloud.com>
2025-11-28 23:11:12 +0400

    doc: update readme and log in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,15 @@ async function bootstrap() {
 
   const port = process.env.PORT || 4000;
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`\n 🚀Application is running on: http://localhost:${port}`);
+  console.log(
+    '\x1b[32m ⚠️ To check tests without authorization, please comment the \x1b[33mTODO:\x1b[32m lines in the files:\x1b[34m',
+    `
+    ./albums/albums.controller.ts 
+    ./artists/artists.controller.ts
+    ./favorites/favorites.controller.ts
+    ./tracks/tracks.controller.ts
+    ./users/users/controller.ts`,
+  );
 }
 bootstrap();
